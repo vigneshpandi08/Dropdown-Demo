b1dbef2118834adc3ea7d0e5ae8c6c504087629d
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-05-15 16:58:05 +0530

    Added drop down sample

---
+++ Dropdown demo/Dropdown demo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Dropdown_demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dropdown_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        TestEntities1 entities = new TestEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult Getcity()
+        {
+            var cityname = entities.CityLists.ToList();
+            return Json(cityname, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ Dropdown demo/Dropdown demo/Models/tb_City.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dropdown_demo.Models
+{
+    public class tb_City
+    {
+    }
+}
